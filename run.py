86fed7679a4f5a555dddf3623d79177344f83c1e
Baris Dincer <81470496+BrsDincer@users.noreply.github.com>
2021-11-23 16:34:42 +0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    
+    if len(sys.argv) >= 1:
+
+        get_info_parameters()
+            
+        
+    elif len(sys.argv) == 0:
+        
+        how_to_use()
+        
+    else:
+        
+        pass
