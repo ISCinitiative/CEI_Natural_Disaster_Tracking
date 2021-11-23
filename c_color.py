1a09a15dbf68afa1a7c3ce7f00a0bab29b207047
Baris Dincer <81470496+BrsDincer@users.noreply.github.com>
2021-11-23 16:24:48 +0300

    Create c_color.py

---
+++ c_color.py
@@ -0,0 +1,14 @@
+class color_select:
+    # for developers
+    # use for define colors within print function,
+    # it could not be running with Windows,
+    # use it for LINUX or other OS instead of Microsoft
+    HEADER_BIG = '\033[95m'
+    BLUE_COLOR = '\033[94m'
+    CYAN_COLOR = '\033[96m'
+    GREEN_COLOR = '\033[92m'
+    WARNING_COLOR = '\033[93m'
+    FAIL_OUT = '\033[91m'
+    ENDC_OUT = '\033[0m'
+    BOLD_OUT = '\033[1m'
+    UNDERLINE_OUT = '\033[4m'
